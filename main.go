246f81977ce1239dc8484f64e6c8d07cd207d693
Prajwal Adhikari <prajwal.adhikari@tekvortex.com>
2024-06-06 14:04:24 +0545

    implement udp server

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"io"
 	"net"
 	"time"
 )
+
+var timeout = flag.Duration("timeout", 15*time.Second, "blocking deadline for read and write")
 
 func main() {
 }
@@ -61,7 +64,57 @@ func client(ctx context.Context, address string, reader io.Reader) (err error) {
 	case <-ctx.Done():
 		fmt.Println("cancelled")
 	case err = <-doneChan:
-
 	}
+
+	return
+}
+
+func server(ctx context.Context, address string) (err error) {
+	pc, err := net.ListenPacket("udp", address)
+	if err != nil {
+		return
+	}
+
+	defer pc.Close()
+
+	doneChan := make(chan error, 1)
+	buffer := make([]byte, maxBufferSize)
+
+	go func() {
+		for {
+			n, addr, err := pc.ReadFrom(buffer)
+			if err != nil {
+				doneChan <- err
+				return
+			}
+
+			fmt.Printf("packet-received: bytes=%d from=%s\n",
+				n, addr.String())
+
+			deadline := time.Now().Add(*timeout)
+			err = pc.SetWriteDeadline(deadline)
+			if err != nil {
+				doneChan <- err
+				return
+			}
+
+			// Write the packet's contents back to the client.
+			n, err = pc.WriteTo(buffer[:n], addr)
+			if err != nil {
+				doneChan <- err
+				return
+			}
+
+			fmt.Printf("packet-written: bytes=%d to=%s\n", n, addr.String())
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("cancelled")
+		err = ctx.Err()
+	case err = <-doneChan:
+	}
+
 	return
 }
